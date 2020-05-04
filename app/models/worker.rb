e1e6e24d7475cd82615efd4b79156f324c1a71a0
SujeevMBP <sujeev@gmail.com>
2020-05-04 18:49:57 +0530

    improved UI

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,4 +3,9 @@ class Worker < ApplicationRecord
 
   validates :email, presence: true
   validates :email, uniqueness: true
+
+  def pretty_time
+    time = self.total_time / 1000
+    return Time.at(time).utc.strftime("%H:%M:%S")
+  end
 end
