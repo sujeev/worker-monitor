e4c64f0954e72feb8e30edefe4d7b5e539469e27
SujeevMBP <sujeev@gmail.com>
2020-05-04 20:24:15 +0530

    started implementing test cases

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
-  # test "should get home" do
-  #   get admin_dashboard_home_url
-  #   assert_response :success
-  # end
+  test "should get home" do
+    get home_admin_dashboard_index_url
+    assert_response :success
+  end
+  test "should get workers" do
+    get workers_admin_dashboard_index_url
+    assert_response :success
+  end
 
 end
